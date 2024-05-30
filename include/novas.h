580d114c96f295565bcd9f7b3eba484a9efb4a12
Attila Kovacs <attipaci@gmail.com>
2024-05-30 12:13:32 +0200

    Set version to 1.0.1-2

--- include/novas.h
+++ include/novas.h
@@ -57,10 +57,10 @@
 #define SUPERNOVAS_MINOR_VERSION  0
 
 /// Integer sub version of the release
-#define SUPERNOVAS_PATCHLEVEL     2
+#define SUPERNOVAS_PATCHLEVEL     1
 
 /// Additional release information in version, e.g. "-1", or "-rc1".
-#define SUPERNOVAS_RELEASE_STRING "-devel"
+#define SUPERNOVAS_RELEASE_STRING "-2"
 
 
 
