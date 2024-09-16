ddbbc0dc5ea2005bd2c29e1135762df18568da0d
Kiran Shila <me@kiranshila.com>
2024-09-16 13:15:06 -0700

    specify the c99 standard for cppcheck

--- config.mk
+++ config.mk
@@ -97,7 +97,7 @@ DEFAULT_SOLSYS ?= 3
 # cppcheck options for 'check' target. You can add additional options by
 # setting the CHECKEXTRA variable (e.g. in shell) prior to invoking 'make'.
 CHECKOPTS ?= --enable=performance,warning,portability,style --language=c \
-            --error-exitcode=1 $(CHECKEXTRA)
+            --error-exitcode=1 --std=c99$(CHECKEXTRA)
 
 # Exhaustive checking for newer cppcheck...
 #CHECKOPTS += --check-level=exhaustive
