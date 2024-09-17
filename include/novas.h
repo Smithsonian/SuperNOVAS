81d7c11ae2d593bad08bee72af51e3db9a2c53ae
Kiran Shila <me@kiranshila.com>
2024-09-17 10:04:00 -0700

    Fixed a bit of formatting

--- include/novas.h
+++ include/novas.h
@@ -24,7 +24,7 @@
 #include <stdint.h>
 #include <time.h>
 
-/// Definition of π in case it's not defined in math.h
+/// Definition of &pi; in case it's not defined in math.h
 #ifndef M_PI
 #    define M_PI 3.14159265358979323846
 #endif
@@ -775,7 +775,7 @@ typedef struct {
  * @see novas_change_observer()
  */
 typedef struct {
-  uint64_t state;                  ///< An internal state for checking validity.
+  uint64_t state;                 ///< An internal state for checking validity.
   enum novas_accuracy accuracy;   ///< NOVAS_FULL_ACCURACY or NOVAS_REDUCED_ACCURACY
   novas_timespec time;            ///< The instant of time for which this observing frame is valid
   observer observer;              ///< The observer location, or NULL for barycentric
