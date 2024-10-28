00e9ccfc4cbb966f4d05b9aaca7ebe1bf989f199
Attila Kovacs <attipaci@gmail.com>
2024-10-28 08:11:59 +0100

    v1.1.1

--- include/novas.h
+++ include/novas.h
@@ -69,7 +69,7 @@
 #define SUPERNOVAS_PATCHLEVEL     2
 
 /// Additional release information in version, e.g. "-1", or "-rc1".
-#define SUPERNOVAS_RELEASE_STRING "-devel"
+#define SUPERNOVAS_RELEASE_STRING ""
 
 /// \cond PRIVATE
 
