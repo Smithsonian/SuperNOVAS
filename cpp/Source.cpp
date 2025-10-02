4fec1e25464ad7baf42b86689ba14dbc819b459c
Attila Kovacs <attipaci@gmail.com>
2025-10-02 16:54:13 +0200

    Fix overall source structure and declarations.

--- cpp/Source.cpp
+++ cpp/Source.cpp
@@ -8,143 +8,139 @@
 #include "supernovas.h"
 
 
-namespace supernovas {
-
-class Source {
-protected:
-  struct novas_object _object = {};
-
-  Source() {}
-
-public:
-
-  const struct novas_object *_novas_object() const {
-    return &_object;
-  }
-
-  std::string name() const {
-    return std::string(_object.name);
-  }
-
-  Apparent apparent(const Frame& frame, enum novas_reference_system system = NOVAS_TOD) const {
-    sky_pos pos = {};
-    novas_sky_pos(&_object, frame._novas_frame(), system, &pos);
-    return Apparent(frame, &pos, system);
-  }
-
-  Geometric geometric(const Frame& frame, enum novas_reference_system system = NOVAS_TOD) {
-    double p[3] = {0.0}, v[3] = {0.0};
-    novas_geom_posvel(&_object, frame._novas_frame(), system, p, v);
-    return Geometric(frame, system,
-            Position(p[0] * Unit::au, p[1] * Unit::au, p[2] * Unit::au),
-            Velocity(v[0] * Unit::au / Unit::day, v[1] * Unit::au / Unit::day, v[2] * Unit::au / Unit::day)
-            );
-  }
-
-  Time rises_above(double el, const Frame &frame, RefractionModel ref, const Weather& weather) const {
-    if(ref)
-      el = Horizontal(0.0, el * Unit::deg).to_unrefracted(frame, ref, weather).elevation().deg();
-    return Time(novas_rises_above(el / Unit::deg, &_object, frame._novas_frame(), NULL), frame.eop());
-  }
-
-  Time transits(const Frame &frame) const {
-    return Time(novas_transit_time(&_object, frame._novas_frame()), frame.eop());
-  }
-
-  Time sets_below(double el, const Frame &frame, RefractionModel ref, const Weather& weather) const {
-    if(ref)
-      el = Horizontal(0.0, el * Unit::deg).to_unrefracted(frame, ref, weather).elevation().deg();
-    return Time(novas_sets_below(el / Unit::deg, &_object, frame._novas_frame(), NULL), frame.eop());
-  }
-
-  Angle sun_angle(const Frame& frame) const {
-    return Angle(novas_sun_angle(&_object, frame._novas_frame()) * Unit::deg);
-  }
-
-  Angle moon_angle(const Frame& frame) const {
-    return Angle(novas_moon_angle(&_object, frame._novas_frame()) * Unit::deg);
-  }
-
-  Angle angle_from(const Source& source, Frame& frame) const {
-    return Angle(novas_object_sep(&_object, &source._object, frame._novas_frame()) * Unit::deg);
-  }
-
-};
+using namespace supernovas;
 
 
-class CatalogSource : public Source {
-private:
-  System _system;
-public:
+const struct novas_object *Source::_novas_object() const {
+  return &_object;
+}
 
-  CatalogSource(const CatalogEntry& e)
-  : Source(), _system(e.system()) {
-    make_cat_object_sys(e._cat_entry(), e.system().c_str(), &_object);
-  }
+std::string Source::name() const {
+  return std::string(_object.name);
+}
 
-  const cat_entry * _cat_entry() const {
-    return &_object.star;
-  }
+Apparent Source::apparent(const Frame& frame, enum novas_reference_system system) const {
+  sky_pos pos = {};
+  novas_sky_pos(&_object, frame._novas_frame(), system, &pos);
+  return Apparent(frame, &pos, system);
+}
 
-  const System& system() const {
-    return _system;
-  }
+Geometric Source::geometric(const Frame& frame, enum novas_reference_system system) const {
+  double p[3] = {0.0}, v[3] = {0.0};
+  novas_geom_posvel(&_object, frame._novas_frame(), system, p, v);
+  return Geometric(frame, system,
+          Position(p[0] * Unit::au, p[1] * Unit::au, p[2] * Unit::au),
+          Velocity(v[0] * Unit::au / Unit::day, v[1] * Unit::au / Unit::day, v[2] * Unit::au / Unit::day)
+  );
+}
 
-  CatalogEntry catalog_entry() const {
-    return CatalogEntry(&_object.star, _object.star.starname);
-  }
+static EOP no_eop = EOP(0, 0.0, 0.0, 0.0);
 
-};
+static const EOP& extract_eop(const Frame &frame) {
+  if(!frame.observer().is_geodetic())
+    return no_eop;
 
+  const GeodeticObserver& eobs = dynamic_cast<const GeodeticObserver&>(frame.observer());
+  return eobs.eop();
+}
 
-class SolarSystemSource : public Source {
-protected:
-  SolarSystemSource() : Source() {}
+Time Source::rises_above(double el, const Frame &frame, RefractionModel ref, const Weather& weather) const {
+  if(ref)
+    el = Horizontal(0.0, el * Unit::deg).to_unrefracted(frame, ref, weather).elevation().deg();
+  return Time(novas_rises_above(el / Unit::deg, &_object, frame._novas_frame(), NULL), extract_eop(frame));
+}
 
-public:
-  double solar_illumination(const Frame& frame) const {
-    return novas_solar_illum(&_object, frame._novas_frame());
-  }
+Time Source::transits(const Frame &frame) const {
+  return Time(novas_transit_time(&_object, frame._novas_frame()), extract_eop(frame));
+}
 
-  double helio_distance(const Time& time, double *rate = NULL) const {
-    return novas_helio_dist(time.jd(NOVAS_TDB), &_object, rate);
-  }
+Time Source::sets_below(double el, const Frame &frame, RefractionModel ref, const Weather& weather) const {
+  if(ref)
+    el = Horizontal(0.0, el * Unit::deg).to_unrefracted(frame, ref, weather).elevation().deg();
+  return Time(novas_sets_below(el / Unit::deg, &_object, frame._novas_frame(), NULL), extract_eop(frame));
+}
 
-  double solar_power(const Time& time) const {
-    return novas_solar_power(time.jd(NOVAS_TDB), &_object);
-  }
-};
+Angle Source::sun_angle(const Frame& frame) const {
+  return Angle(novas_sun_angle(&_object, frame._novas_frame()) * Unit::deg);
+}
+
+Angle Source::moon_angle(const Frame& frame) const {
+  return Angle(novas_moon_angle(&_object, frame._novas_frame()) * Unit::deg);
+}
+
+Angle Source::angle_to(const Source& source, const Frame& frame) const {
+  return Angle(novas_object_sep(&_object, &source._object, frame._novas_frame()) * Unit::deg);
+}
 
 
 
-class Planet : public SolarSystemSource {
-public:
-  Planet(enum novas_planet number) : SolarSystemSource() {
-    make_planet(number, &_object);
-  }
-
-  Planet(const std::string& name) : SolarSystemSource() {
-    make_planet(novas_planet_for_name(name.c_str()), &_object);
-  }
-};
 
 
-class EphemerisSource : public SolarSystemSource {
-public:
-  EphemerisSource(const std::string &name, long number) : SolarSystemSource() {
-    make_ephem_object(name.c_str(), number, &_object);
-  }
-};
 
 
-class OrbitalSource : public SolarSystemSource {
-public:
-  OrbitalSource(const std::string& name, long number, const novas_orbital *orbit) : SolarSystemSource() {
-    make_orbital_object(name.c_str(), number, orbit, &_object);
-  }
-};
+CatalogSource::CatalogSource(const CatalogEntry& e)
+: Source(), _system(e.system()) {
+  make_cat_object_sys(e._cat_entry(), e.system().c_str(), &_object);
+}
+
+const cat_entry * CatalogSource::_cat_entry() const {
+  return &_object.star;
+}
+
+const System& CatalogSource::system() const {
+  return _system;
+}
+
+CatalogEntry CatalogSource::catalog_entry() const {
+  return CatalogEntry(&_object.star, _object.star.starname);
+}
 
 
-} // namespace supernovas
+
+
+
+double SolarSystemSource::solar_illumination(const Frame& frame) const {
+  return novas_solar_illum(&_object, frame._novas_frame());
+}
+
+double SolarSystemSource::helio_distance(const Time& time, double *rate) const {
+  return novas_helio_dist(time.jd(NOVAS_TDB), &_object, rate);
+}
+
+double SolarSystemSource::solar_power(const Time& time) const {
+  return novas_solar_power(time.jd(NOVAS_TDB), &_object);
+}
+
+
+
+
+
+
+Planet::Planet(enum novas_planet number) : SolarSystemSource() {
+  make_planet(number, &_object);
+}
+
+Planet::Planet(const std::string& name) : SolarSystemSource() {
+  make_planet(novas_planet_for_name(name.c_str()), &_object);
+}
+
+
+
+
+
+
+
+EphemerisSource::EphemerisSource(const std::string &name, long number) : SolarSystemSource() {
+  make_ephem_object(name.c_str(), number, &_object);
+}
+
+
+
+
+OrbitalSource::OrbitalSource(const std::string& name, long number, const novas_orbital *orbit) : SolarSystemSource() {
+  make_orbital_object(name.c_str(), number, orbit, &_object);
+}
+
+
 
 
--- cpp/Time.cpp
+++ cpp/Time.cpp
@@ -8,81 +8,69 @@
 
 #include "supernovas.h"
 
-namespace supernovas {
-
-class Time {
-protected:
-  novas_timespec _ts = {};
-
-  Time();
-
-public:
-
-  Time(double jd, const EOP& eop, enum novas_timescale timescale = NOVAS_TT) {
-    novas_set_time(timescale, jd, eop.leap_seconds(), eop.dUT1(), &_ts);
-  }
-
-  Time(const std::string& timestamp, const EOP& eop, enum novas_timescale timescale = NOVAS_UTC) {
-    novas_set_str_time(timescale, timestamp.c_str(), eop.leap_seconds(), eop.dUT1(), &_ts);
-  }
-
-  Time(const struct timespec *t, const EOP& eop) {
-    novas_set_unix_time(t->tv_sec, t->tv_nsec, eop.leap_seconds(), eop.dUT1(), &_ts);
-  }
-
-  Time(const novas_timespec *t) : _ts(*t) {}
-
-  Interval operator-(const Time &other) const {
-    return Interval(novas_diff_time(&_ts, &other._ts), NOVAS_TT);
-  }
-
-  const novas_timespec * _novas_timespec() const {
-    return &_ts;
-  }
-
-  double jd(enum novas_timescale timescale = NOVAS_TT) const {
-    return novas_get_time(&_ts, timescale);
-  }
-
-  double epoch() const {
-    return 2000.0 + (jd() - NOVAS_JD_J2000) / NOVAS_JULIAN_YEAR_DAYS;
-  }
-
-  TimeAngle time_of_day(enum novas_timescale timescale = NOVAS_TT) const {
-    return TimeAngle(TWOPI * novas_get_split_time(&_ts, timescale, NULL));
-  }
-
-  double day_of_week(enum novas_timescale timescale = NOVAS_TT) const {
-    return novas_day_of_week(jd(timescale));
-  }
-
-  std::string str(enum novas_timescale timescale = NOVAS_UTC) const {
-    char str[40] = {'\0'};
-    novas_timestamp(&_ts, timescale, str, sizeof(str));
-    return std::string(str);
-  }
-
-  std::string iso_str() const {
-    char str[40] = {'\0'};
-    novas_iso_timestamp(&_ts, str, sizeof(str));
-    return std::string(str);
-  }
-
-  std::string epoch_str() const {
-    char s[40] = {'\0'};
-    snprintf(s, sizeof(s), "J%.2f", epoch());
-    return std::string(s);
-  }
-
-  static Time now(const EOP& eop) {
-    Time time = Time();
-    novas_set_current_time(eop.leap_seconds(), eop.dUT1(), &time._ts);
-    return time;
-  }
+using namespace supernovas;
 
 
-};
+Time::Time(double jd, const EOP& eop, enum novas_timescale timescale) {
+  novas_set_time(timescale, jd, eop.leap_seconds(), eop.dUT1(), &_ts);
+}
 
+Time::Time(const std::string& timestamp, const EOP& eop, enum novas_timescale timescale) {
+  novas_set_str_time(timescale, timestamp.c_str(), eop.leap_seconds(), eop.dUT1(), &_ts);
+}
 
-} // namespace supernovas
+Time::Time(const struct timespec *t, const EOP& eop) {
+  novas_set_unix_time(t->tv_sec, t->tv_nsec, eop.leap_seconds(), eop.dUT1(), &_ts);
+}
+
+Time::Time(const novas_timespec *t) : _ts(*t) {}
+
+const novas_timespec * Time::_novas_timespec() const {
+  return &_ts;
+}
+
+double Time::jd(enum novas_timescale timescale) const {
+  return novas_get_time(&_ts, timescale);
+}
+
+double Time::epoch() const {
+  return 2000.0 + (jd() - NOVAS_JD_J2000) / NOVAS_JULIAN_YEAR_DAYS;
+}
+
+TimeAngle Time::time_of_day(enum novas_timescale timescale) const {
+  return TimeAngle(TWOPI * novas_get_split_time(&_ts, timescale, NULL));
+}
+
+int Time::day_of_week(enum novas_timescale timescale) const {
+  return novas_day_of_week(jd(timescale));
+}
+
+std::string Time::str(enum novas_timescale timescale) const {
+  char str[40] = {'\0'};
+  novas_timestamp(&_ts, timescale, str, sizeof(str));
+  return std::string(str);
+}
+
+std::string Time::iso_str() const {
+  char str[40] = {'\0'};
+  novas_iso_timestamp(&_ts, str, sizeof(str));
+  return std::string(str);
+}
+
+std::string Time::epoch_str() const {
+  char s[40] = {'\0'};
+  snprintf(s, sizeof(s), "J%.2f", epoch());
+  return std::string(s);
+}
+
+Time Time::now(const EOP& eop) {
+  Time time = Time();
+  novas_set_current_time(eop.leap_seconds(), eop.dUT1(), &time._ts);
+  return time;
+}
+
+Interval operator-(const Time& l, const Time &r) {
+  return Interval(novas_diff_time(l._novas_timespec(), r._novas_timespec()), NOVAS_TT);
+}
+
 
