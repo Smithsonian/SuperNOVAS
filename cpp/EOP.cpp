74de846702501ac62c52eb3e251cd2a46fb10044
Attila Kovacs <attipaci@gmail.com>
2025-10-02 16:54:13 +0200

    Fix overall source structure and declarations.

--- cpp/EOP.cpp
+++ cpp/EOP.cpp
@@ -8,59 +8,45 @@
 #include "supernovas.h"
 
 
-namespace supernovas {
+using namespace supernovas;
 
-class EOP {
-private:
-  int _leap = 0;
-  Angle _xp, _yp;
-  double _t;
+EOP::EOP(int leap_seconds, double dut1, double xp, double yp)
+: _leap(leap_seconds), _xp(xp), _yp(yp), _t(dut1) {}
 
-  double _dxp = 0.0, _dyp = 0.0, _dt = 0.0;  // [mas, s] Applied corrections, in novas units.
+EOP::EOP(int leap_seconds, double dut1, const Angle& xp, const Angle& yp)
+: EOP(leap_seconds, dut1, xp.rad(), yp.rad()) {}
 
-public:
+int EOP::leap_seconds() const {
+  return _leap;
+}
 
-  EOP(int leap_seconds, double dut1 = 0.0, double xp = 0.0, double yp = 0.0)
-  : _leap(leap_seconds), _xp(xp), _yp(yp), _t(dut1) {}
+const Angle& EOP::xp() const {
+  return _xp;
+}
 
-  EOP(int leap_seconds, double dut1, const Angle& xp, const Angle& yp)
-  : EOP(leap_seconds, dut1, xp.rad(), yp.rad()) {}
+const Angle& EOP::yp() const {
+  return _yp;
+}
 
-  int leap_seconds() const {
-    return _leap;
-  }
+double EOP::dUT1() const {
+  return _t;
+}
 
-  const Angle& xp() const {
-    return _xp;
-  }
+EOP EOP::itrf_transformed(int from_year, int to_year) const {
+  double xp1, yp1, t1;
+  novas_itrf_transform_eop(from_year, _xp.arcsec(), _yp.arcsec(), _t, to_year, &xp1, &yp1, &t1);
+  return EOP(_leap, t1, xp1 * Unit::arcsec, yp1 * Unit::arcsec);
+}
 
-  const Angle& yp() const {
-    return _yp;
-  }
+EOP EOP::diurnal_corrected(const Time& time) const {
+  double dxp, dyp, dt;
+  novas_diurnal_eop_at_time(time._novas_timespec(), &dxp, &dyp, &dt);
 
-  double dUT1() const {
-    return _t;
-  }
+  // Apply correction rel. to prior correction.
+  EOP eop = EOP(_leap, _t + dt - _dt, (_xp.rad() + dxp - _dxp) * Unit::arcsec, (_yp.rad() + dyp - _dyp) * Unit::arcsec);
 
-  EOP itrf_transformed(int from_year, int to_year) const {
-    double xp1, yp1, t1;
-    novas_itrf_transform_eop(from_year, _xp.arcsec(), _yp.arcsec(), _t, to_year, &xp1, &yp1, &t1);
-    return EOP(_leap, t1, xp1 * Unit::arcsec, yp1 * Unit::arcsec);
-  }
-
-  EOP diurnal_corrected(const Time& time) const {
-    double dxp, dyp, dt;
-    novas_diurnal_eop_at_time(time._novas_timespec(), &dxp, &dyp, &dt);
-
-    // Apply correction rel. to prior correction.
-    EOP eop = EOP(_leap, _t + dt - _dt, (_xp.rad() + dxp - _dxp) * Unit::arcsec, (_yp.rad() + dyp - _dyp) * Unit::arcsec);
-
-    eop._dxp = dxp;
-    eop._dyp = dyp;
-    eop._dt = dt;
-    return eop;
-  }
-};
-} // namespace supernovas
-
-
+  eop._dxp = dxp;
+  eop._dyp = dyp;
+  eop._dt = dt;
+  return eop;
+}
--- cpp/Horizontal.cpp
+++ cpp/Horizontal.cpp
@@ -8,69 +8,63 @@
 #include "supernovas.h"
 
 
-namespace supernovas {
+using namespace supernovas;
 
-class Horizontal: public Spherical {
-private:
-
-  int location_with_weather(const Frame& frame, const Weather& weather, on_surface *s) {
-    s->temperature = weather.temperature().celsius();
-    s->pressure = weather.pressure().mbar();
-    s->humidity = weather.humidity();
-    return 0;
-  }
-
-public:
-  Horizontal(double azimuth, double elevation, double distance = NOVAS_DEFAULT_DISTANCE)
-  : Spherical(azimuth, elevation, distance) {}
-
-  Horizontal(const Angle& azimuth, const Angle& elevation, double distance = NOVAS_DEFAULT_DISTANCE)
-  : Spherical(azimuth, elevation, distance) {}
-
-  Horizontal(const Angle& azimuth, const Angle& elevation, const Distance& distance)
-  : Spherical(azimuth, elevation, distance) {}
-
-  const Angle& azimuth() const {
-    return longitude();
-  }
-
-  const Angle& elevation() const {
-    return latitude();
-  }
-
-  Horizontal to_refracted(const Frame &frame, RefractionModel ref, const Weather& weather) {
-    on_surface loc = {};
-    location_with_weather(frame, weather, &loc);
-    double del = ref ? ref(frame.time().jd(), &loc, NOVAS_REFRACT_ASTROMETRIC, elevation().deg()) : 0.0;
-    return Horizontal(longitude().rad(), latitude().rad() + del * Unit::arcsec);
-  }
-
-  Horizontal to_unrefracted(const Frame &frame, RefractionModel ref, const Weather& weather) {
-    on_surface loc = {};
-    location_with_weather(frame, weather, &loc);
-    double del = ref ? ref(frame.time().jd(), &loc, NOVAS_REFRACT_OBSERVED, elevation().deg()) : 0.0;
-    return Horizontal(longitude().rad(), latitude().rad() - del * Unit::arcsec);
-  }
-
-  Apparent to_apparent(const Frame& frame, double rv = 0.0, double distance = 0.0) const {
-    sky_pos p = {};
-    novas_hor_to_app(frame._novas_frame(), _lon.deg(), _lat.deg(), NULL, NOVAS_TOD, &p.ra, &p.dec);
-    p.rv = rv * Unit::au / Unit::day;
-    p.dis = distance * Unit::au;
-    radec2vector(p.ra, p.dec, 1.0, p.r_hat);
-    return Apparent(frame, &p, NOVAS_TOD);
-  }
-
-  Apparent to_apparent(const Frame& frame, Speed& rv, Distance& distance) const {
-    return to_apparent(frame, rv.ms(), distance.m());
-  }
-
-  const std::string str(enum novas_separator_type separator = NOVAS_SEP_UNITS_AND_SPACES, int decimals = 3) const {
-    return "HOR  " + Spherical::str(separator, decimals);
-  }
-};
+int Horizontal::location_with_weather(const Frame& frame, const Weather& weather, on_surface *s) {
+  s->temperature = weather.temperature().celsius();
+  s->pressure = weather.pressure().mbar();
+  s->humidity = weather.humidity();
+  return 0;
+}
 
 
-} // namespace supernovas
+Horizontal::Horizontal(double azimuth, double elevation, double distance)
+: Spherical(azimuth, elevation, distance) {}
+
+Horizontal::Horizontal(const Angle& azimuth, const Angle& elevation, double distance)
+: Spherical(azimuth, elevation, distance) {}
+
+Horizontal::Horizontal(const Angle& azimuth, const Angle& elevation, const Distance& distance)
+: Spherical(azimuth, elevation, distance) {}
+
+const Angle& Horizontal::azimuth() const {
+  return longitude();
+}
+
+const Angle& Horizontal::elevation() const {
+  return latitude();
+}
+
+Horizontal Horizontal::to_refracted(const Frame &frame, RefractionModel ref, const Weather& weather) {
+  on_surface loc = {};
+  location_with_weather(frame, weather, &loc);
+  double del = ref ? ref(frame.time().jd(), &loc, NOVAS_REFRACT_ASTROMETRIC, elevation().deg()) : 0.0;
+  return Horizontal(longitude().rad(), latitude().rad() + del * Unit::arcsec);
+}
+
+Horizontal Horizontal::to_unrefracted(const Frame &frame, RefractionModel ref, const Weather& weather) {
+  on_surface loc = {};
+  location_with_weather(frame, weather, &loc);
+  double del = ref ? ref(frame.time().jd(), &loc, NOVAS_REFRACT_OBSERVED, elevation().deg()) : 0.0;
+  return Horizontal(longitude().rad(), latitude().rad() - del * Unit::arcsec);
+}
+
+Apparent Horizontal::to_apparent(const Frame& frame, double rv, double distance) const {
+  sky_pos p = {};
+  novas_hor_to_app(frame._novas_frame(), _lon.deg(), _lat.deg(), NULL, NOVAS_TOD, &p.ra, &p.dec);
+  p.rv = rv * Unit::au / Unit::day;
+  p.dis = distance * Unit::au;
+  radec2vector(p.ra, p.dec, 1.0, p.r_hat);
+  return Apparent(frame, &p, NOVAS_TOD);
+}
+
+Apparent Horizontal::to_apparent(const Frame& frame, Speed& rv, Distance& distance) const {
+  return to_apparent(frame, rv.ms(), distance.m());
+}
+
+const std::string Horizontal::str(enum novas_separator_type separator, int decimals) const {
+  return "HOR  " + Spherical::str(separator, decimals);
+}
+
 
 
