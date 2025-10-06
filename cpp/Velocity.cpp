a289e957b72d43402794ab8999e97801bf931ab7
Attila Kovacs <attipaci@gmail.com>
2025-10-06 15:54:26 +0200

    A few more tweaks for today.

--- cpp/Velocity.cpp
+++ cpp/Velocity.cpp
@@ -16,23 +16,29 @@ namespace supernovas {
 
 Velocity::Velocity(double x_ms, double y_ms, double z_ms)
 : Vector(x_ms, y_ms, z_ms) {
+  static const char *fn = "Velocity()";
+
   if(!_valid)
-    novas::novas_trace_invalid("Velocity()");
+    novas::novas_trace_invalid(fn);
+  else if(abs() > Constant::c) {
+    novas::novas_error(0, ERANGE, fn, "input velocity exceeds the speed of light: %s", str().c_str());
+    _valid = false;
+  }
 }
 
 Velocity::Velocity(const double vel[3], double unit)
 : Vector(vel[0] * unit, vel[1] * unit, vel[2] * unit) {}
 
 
-double Velocity::x_ms() const {
+double Velocity::x_m_per_s() const {
   return _component[0];
 }
 
-double Velocity::y_ms() const {
+double Velocity::y_m_per_s() const {
   return _component[1];
 }
 
-double Velocity::z_ms() const {
+double Velocity::z_m_per_s() const {
   return _component[2];
 }
 
@@ -49,7 +55,7 @@ Position Velocity::travel(const Interval& t) const {
 }
 
 Position Velocity::travel(double seconds) const {
-  return Position(x_ms() * seconds, y_ms() * seconds, z_ms() * seconds);
+  return Position(x_m_per_s() * seconds, y_m_per_s() * seconds, z_m_per_s() * seconds);
 }
 
 Velocity Velocity::inv() const {
@@ -68,15 +74,15 @@ static double v_add(double v1, double v2) {
 }
 
 std::string Velocity::str() const {
-  return "VEL ( " + Speed(x_ms()).str() + ", " + Speed(y_ms()).str() + ", " + Speed(z_ms()).str() + ")";
+  return "VEL ( " + Speed(x_m_per_s()).str() + ", " + Speed(y_m_per_s()).str() + ", " + Speed(z_m_per_s()).str() + ")";
 }
 
 Velocity operator+(const Velocity& l, const Velocity& r) {
-  return Velocity(v_add(l.x_ms(), r.x_ms()), v_add(l.y_ms(), r.y_ms()), v_add(l.z_ms(), r.z_ms()));
+  return Velocity(v_add(l.x_m_per_s(), r.x_m_per_s()), v_add(l.y_m_per_s(), r.y_m_per_s()), v_add(l.z_m_per_s(), r.z_m_per_s()));
 }
 
 Velocity operator-(const Velocity& l, const Velocity& r) {
-  return Velocity(v_add(l.x_ms(), -r.x_ms()), v_add(l.y_ms(), -r.y_ms()), v_add(l.z_ms(), -r.z_ms()));
+  return Velocity(v_add(l.x_m_per_s(), -r.x_m_per_s()), v_add(l.y_m_per_s(), -r.y_m_per_s()), v_add(l.z_m_per_s(), -r.z_m_per_s()));
 }
 
 static const Velocity _nan = Velocity(NAN, NAN, NAN);
