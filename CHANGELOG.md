71751c7bf268c2d15f8bdedb9de87370aa335db9
Attila Kovacs <attipaci@gmail.com>
2026-01-02 13:52:57 +0100

    Back to devel version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,18 @@ file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/), and this project adheres to 
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+Upcoming feature release.
+
+### Fixed
+
+
+### Added
+
+
+### Changed
+
 
 ## [1.5.1] - 2026-01-02
 
--- include/novas.h
+++ include/novas.h
@@ -70,13 +70,13 @@
 #define SUPERNOVAS_MAJOR_VERSION  1
 
 /// API minor version
-#define SUPERNOVAS_MINOR_VERSION  5
+#define SUPERNOVAS_MINOR_VERSION  6
 
 /// Integer sub version of the release
-#define SUPERNOVAS_PATCHLEVEL     1
+#define SUPERNOVAS_PATCHLEVEL     0
 
 /// Additional release information in version, e.g. "-1", or "-rc1", or empty string "" for releases.
-#define SUPERNOVAS_RELEASE_STRING ""
+#define SUPERNOVAS_RELEASE_STRING "-devel"
 
 /// \cond PRIVATE
 
